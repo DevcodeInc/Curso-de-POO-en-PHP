37c8e70e343b4a853d1cd2ebf991c9d404f5e130
martinartaza <martin.artaza@gmail.com>
2017-07-28 00:54:12 -0300

    Metodos get y set

--- imagen.php
+++ imagen.php
@@ -5,8 +5,37 @@ namespace Devcodela;
 class Imagen
 {
     public $url;
+    public $ancho;
     public function mostrar()
     {
-        return "<img src=\"" . $this->url . "\">"; // <img src="ejemplo.jpg">
+        return "<img src=\"" . $this->url . "\" width=\"" . $this->ancho ."\">"; // <img src="ejemplo.jpg">
+    }
+
+    public function __construct()
+    {
+        $this->url = "image_not_found.png";
+    }
+
+    public function setAncho($nuevoAncho)
+    {
+        try {
+            if (!is_int($nuevoAncho)) {
+                throw new \Exception("El ancho debe ser un numero entero", 1);
+            } else if ($nuevoAncho == 0) {
+                throw new \Exception("El ancho debe ser un numero distinto a cero", 1);
+            } else if ($nuevoAncho < 0) {
+                throw new \Exception("El ancho debe ser un numero mayor a cero", 1);
+            } else {
+                $this->ancho = $nuevoAncho;
+            }   
+        } catch (\Exception $e) {
+            $file = fopen("/var/www/html/poo/log.txt", "a");
+            fwrite($file , $e->getMessage() . PHP_EOL);
+            fclose($file);
+        }
+    }
+
+    public function getAncho() {
+        return $this->ancho;
     }
 }
--- index.php
+++ index.php
@@ -1,9 +1,19 @@
+<!DOCTYPE html>
 <html>
-<?php require "imagen.php" ?>
+	<?php 
+	error_reporting(E_ALL);
+	ini_set("display_errors", 1);
+	require "imagen.php";
+	?>
+
 <body>
 	Hola amigos devcodela
 	<?php $mi_objeto_imagen = new Devcodela\imagen();
 	$mi_objeto_imagen->url = 'ejemplo.jpg';
+	
+	$ancho = 50;
+	$mi_objeto_imagen->ancho = $ancho;
+
 	echo $mi_objeto_imagen->mostrar();
 	var_dump($mi_objeto_imagen);
 	?>
